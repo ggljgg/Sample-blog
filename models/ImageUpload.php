c9b5c58e4c1755ec43f4805baf6324d3db874011
ggljgg <luidjy75@gmail.com>
2018-11-11 16:37:39 +0300

    Создана дополнительная модель ImageUpload

---
+++ models/ImageUpload.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\models;
+
+use yii\base\Model;
+
+/**
+ * This is class for uploading image.
+ * 
+ * @property string $image
+ */
+class ImageUpload extends Model
+{
+    public $image;
+}
